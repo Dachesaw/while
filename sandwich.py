e92cd55af13f04847165895e205e75e8449f7069
Kaoru <topgearchesak@gmail.com>
2023-05-02 19:29:24 +0300

    remove 'сырный' sandwith

--- sandwich.py
+++ sandwich.py
@@ -1,10 +1,13 @@
-sandwith_orders = ['cырный', 'рыбный', 'двойной', 'летний', 'с маслом']
+sandwith_orders = ['сырный', 'рыбный', 'сырный', 'двойной', 'сырный', 'летний', 'с маслом']
 finished_sandwith = []
 
+while 'сырный' in sandwith_orders:
+    sandwith_orders.remove('сырный')
+print(sandwith_orders)
 while sandwith_orders:
     sandwith = sandwith_orders.pop()
-    print(f'Заказали {sandwith} сэндвитч')
+    print(f'Заказали сэндвитч {sandwith}')
     finished_sandwith.append(sandwith)
 print('\n')
 for sandwith in finished_sandwith:
-    print(f'Приготовили {sandwith} сэндвич')
+    print(f'Приготовили сэндвитч {sandwith}')
